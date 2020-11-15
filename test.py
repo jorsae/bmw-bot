426fc376c24fdb25d6daf93a03e75badfdd0d8d0
jorsae <jorgen.sater@gmail.com>
2020-11-15 17:52:33 +1100

    Removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-GET_USER = re.compile('@!?\d{17,18}')
-
-a = '<@!89452245153779712>! '
-b = 'congratulations <@300744899563618306>! you caught a level 27 gastly!'
-
-print(GET_USER.search(a))
-print(GET_USER.search(b))
