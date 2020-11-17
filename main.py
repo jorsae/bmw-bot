a9611804c81540120afc7885fa4ddb38b6f1664d
jorsae <jorgen.sater@gmail.com>
2020-11-17 11:14:46 +1100

    Fixed issue in change_status if uery.get_pokemon_caught returns None

--- main.py
+++ main.py
@@ -145,6 +145,7 @@ def setup_logging():
 @tasks.loop(seconds=600, reconnect=True)
 async def change_status():
     total_caught = await query.get_pokemon_caught(this_month=True)
+    total_caught = 0 if total_caught is None else total_caught
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{total_caught:,} pokémon caught'))
 
 @bot.event
